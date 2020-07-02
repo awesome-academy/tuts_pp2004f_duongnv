c47cb72df708b638532fd62f0c01846011ce84eb
Duong <nguyenduong300990@gmail.com>
2020-07-02 16:42:07 +0700

    first_website

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('home','PagesController@home');
+Route::get('about','PagesController@about');
+Route::get('contact','PagesController@contact');
